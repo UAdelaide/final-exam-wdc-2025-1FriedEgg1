dac73415f32f1b70dc39d49d054d02ca53b1f130
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:49:56 +0000

    [automatic commit] 2025-06-20 01:49:56 UTC 119.40.108.209

--- part1/app.js
+++ part1/app.js
@@ -45,8 +45,13 @@ INSERT INTO Dogs (name, size, owner_id) SELECT "Max", "medium", user_id FROM Use
 `);
     await db.execute(`
 INSERT INTO Dogs (name, size, owner_id) SELECT "Bella", "small", user_id FROM Users WHERE username = 'carol123';
+`);
+    await db.execute(`
 INSERT INTO Dogs (name, size, owner_id) SELECT "Dog1", "large", user_id FROM Users WHERE username = 'bobwalker';
+`);
+    await db.execute(`
 INSERT INTO Dogs (name, size, owner_id) SELECT "Dog2", "medium", user_id FROM Users WHERE username = 'alice123';
+
 INSERT INTO Dogs (name, size, owner_id) SELECT "Dog3", "small", user_id FROM Users WHERE username = 'steve2';
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) SELECT dog_id, '2025-06-10 08:00:00', 30, "Parklands", "open" FROM Dogs WHERE name="Max";
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) SELECT dog_id, '2025-06-10 09:30:00', 45, "Beachside Ave", "accepted" FROM Dogs WHERE name="Bella";
