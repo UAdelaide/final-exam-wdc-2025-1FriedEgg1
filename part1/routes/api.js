65626b554327bca5e85c526e6f3edabdec52fd72
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:54:38 +0000

    [automatic commit] 2025-06-20 01:54:38 UTC 119.40.108.209

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,7 @@ const db = require("../app");
 
 router.get('/dogs', async function (req, res, next) {
     const [rows] = await db.execute('SELECT d.name, d.size, u.username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id;');
-    return res.json(JSON.stringify(rows));
+    return res.json(rows);
 });
 
 module.exports = router;
