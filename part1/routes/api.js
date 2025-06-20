560e79b17ae5fedf602b34e4d32d00b7b840cbaf
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:28:44 +0000

    [automatic commit] 2025-06-20 01:28:44 UTC 119.40.108.209

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/dogs', function(req, res, next) {
+  const 
 });
 
 module.exports = router;
