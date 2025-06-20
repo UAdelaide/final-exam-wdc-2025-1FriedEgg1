d6d7b1419587560a9b1c75cdbf469adf3e35c1c7
nathanl <a1922438@adelaide.edu.au>
2025-06-20 02:01:42 +0000

    [automatic commit] 2025-06-20 02:01:42 UTC 119.40.108.209

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'DogWalkService',
+    database: 'DogWalkService'
 });
 
 module.exports = db;
