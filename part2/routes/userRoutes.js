0ada59cf8e51ab28fe47a4653a1bbde00216450a
nathanl <a1922438@adelaide.edu.au>
2025-06-20 05:59:15 +0000

    [automatic commit] 2025-06-20 05:59:15 UTC 119.40.108.209

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -69,7 +69,7 @@ router.post('/logout', async (req, res) => {
 
 router.post('/getDogs', async (req, res) => {
   if (!req.session.user) return res.status(401).json({ msg: "User not logged in." });
-  const own
+  const owner_id = req.session.user.user_id;
 });
 
 module.exports = router;
