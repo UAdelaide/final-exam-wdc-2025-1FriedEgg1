de6c78b7688193d9d04a123c8ddb68318ccf7bb2
nathanl <a1922438@adelaide.edu.au>
2025-06-20 02:16:31 +0000

    [automatic commit] 2025-06-20 02:16:31 UTC 119.40.108.209

--- part1/app.js
+++ part1/app.js
@@ -89,7 +89,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async(req, res => {
   try {
-    const [rows] = await db.query('SELECT wr.request_id, d.name, wr.requested_time, wr.duration_minutes, wr.location, u.username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id;');
+    const [rows] = await db.query('SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id;');
     return res.json(rows);
   } catch (err) {
     console.error(err);
