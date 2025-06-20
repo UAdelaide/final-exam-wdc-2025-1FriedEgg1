972e1e496b638d2380bfb55344d3be05f396ce5c
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:41:51 +0000

    [automatic commit] 2025-06-20 01:41:51 UTC 119.40.108.209

--- part1/db.js
+++ part1/db.js
@@ -4,12 +4,6 @@ let db;
 
 (async () => {
     try {
-        // Connect to MySQL without specifying a database
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '' // Set your MySQL root password
-        });
 
         // Create the database if it doesn't exist
 
