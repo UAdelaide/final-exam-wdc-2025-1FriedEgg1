34f99a44e7d331485befdbc1c53f0bb768c6902f
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:28:24 +0000

    [automatic commit] 2025-06-20 01:28:24 UTC 119.40.108.209

--- part1/app.js
+++ part1/app.js
@@ -47,7 +47,7 @@ let db;
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
-    await db.execute(`
+      await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES ("alice123", "alice@example.com", "hashed123", "owner"), ("bobwalker", "bob@example.com", "hashed456", "walker"), ("carol123", "carol@example.com", "hashed789", "owner"), ("mrsteve", "steve@example.com", "steve123", "walker"), ("steve2", "steve2@example.com", "password", "owner");
 
 INSERT INTO Dogs (name, size, owner_id) SELECT "Max", "medium", user_id FROM Users WHERE username = 'alice123';
@@ -70,6 +70,7 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) SELECT dog_id, '2025-06-14 11:30:00', 55, "Langles House", "accepted" FROM Dogs WHERE name="Dog3";
       `);
+    }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
