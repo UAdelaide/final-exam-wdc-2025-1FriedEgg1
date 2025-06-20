c13c6f35dbbf82958e71ac9c83dc8c565409a054
nathanl <a1922438@adelaide.edu.au>
2025-06-20 02:08:06 +0000

    [automatic commit] 2025-06-20 02:08:06 UTC 119.40.108.209

--- part1/app.js
+++ part1/app.js
@@ -89,6 +89,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async(req, res => {
   try {
+    const [rows] = await db.query();
 
   } catch (err) {
     console.error(err);
