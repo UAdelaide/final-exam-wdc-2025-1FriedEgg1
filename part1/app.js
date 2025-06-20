d4a2d15579a29a356123d394367bf082010fa92a
nathanl <a1922438@adelaide.edu.au>
2025-06-20 01:42:52 +0000

    [automatic commit] 2025-06-20 01:42:52 UTC 119.40.108.209

--- part1/app.js
+++ part1/app.js
@@ -21,8 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var mysql = require('mysql2/promise');
-
 let db;
 
 (async () => {
